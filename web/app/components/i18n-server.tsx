4a5c8ef1841a5e4219d8b31a9a9376d1bcf99895
andrew.zhao <andrew.zhao@wayz.ai>
2023-06-13 12:14:56 +0800

    1

--- web/app/components/i18n-server.tsx
+++ web/app/components/i18n-server.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Script from 'next/script'
 import I18N from './i18n'
 import { ToastProvider } from './base/toast'
 import { getDictionary, getLocaleOnServer } from '@/i18n/server'
@@ -17,6 +18,18 @@ const I18NServer = async ({
 
   return (
     <I18N {...{ locale, dictionary }}>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+          ga('create', 'G-9WTD09KFGD', 'auto');
+          ga('send', 'pageview');
+        `}
+      </Script>
+      <Script
+        src="https://www.google-analytics.com/analytics.js"
+        strategy="afterInteractive"
+      />
+
       <ToastProvider>{children}</ToastProvider>
     </I18N>
   )
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,22 +1,9 @@
 import Link from 'next/link'
-import Script from 'next/script'
 import Loading from '@/app/components/base/loading'
 
 const Home = async () => {
   return (
     <div className="flex flex-col justify-center min-h-screen py-12 sm:px-6 lg:px-8">
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-          ga('create', 'G-9WTD09KFGD', 'auto');
-          ga('send', 'pageview');
-        `}
-      </Script>
-      <Script
-        src="https://www.google-analytics.com/analytics.js"
-        strategy="afterInteractive"
-      />
-
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Loading type='area' />
         <div className="mt-10 text-center">
