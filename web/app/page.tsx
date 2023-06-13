b731c4dacd6ca6ebf2e5f7d499f21763bd07351b
andrew.zhao <andrew.zhao@wayz.ai>
2023-06-13 12:05:12 +0800

    1

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -5,19 +5,17 @@ import Loading from '@/app/components/base/loading'
 const Home = async () => {
   return (
     <div className="flex flex-col justify-center min-h-screen py-12 sm:px-6 lg:px-8">
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-9WTD09KFGD"
-        strategy="afterInteractive"
-      />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-9WTD09KFGD');
+          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+          ga('create', 'G-9WTD09KFGD', 'auto');
+          ga('send', 'pageview');
         `}
       </Script>
+      <Script
+        src="https://www.google-analytics.com/analytics.js"
+        strategy="afterInteractive"
+      />
 
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Loading type='area' />
